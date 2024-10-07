e5632e714ab26de3ec888523e3d943c8816ed918
Joel Kasisi <joelkasisi10@gmail.com>
2024-10-07 19:50:51 +0200

    fix

---
+++ server/globals.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      DB_NAME: string;
+      DB_USER: string;
+      DB_PASSWORD: string;
+      DB_HOST: string;
+      DB_PORT: string;
+      JWT_SECRET: string;
+    }
+  }
+  
