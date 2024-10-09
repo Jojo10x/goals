b8456dbe07719d85a018a0f7f51f40ec0fb745cf
Joel Kasisi <joelkasisi10@gmail.com>
2024-10-09 16:15:51 +0200

    added loader

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import{ AuthProvider } from './contexts/AuthContext.tsx'
+import { Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
+import Loader from "./components/Loader.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
-)
+createRoot(document.getElementById("root")!).render(
+  <Suspense fallback={<Loader/>}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Suspense>
+);
